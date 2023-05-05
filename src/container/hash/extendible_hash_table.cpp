572f745165175e6fe0be56fa89f7dd2282e20bca
AyoubKaz07 <a.kazar@esi-sba.dz>
2023-05-05 21:39:41 +0200

    P1 not finished

---
+++ src/container/hash/extendible_hash_table.cpp
@@ -0,0 +1,191 @@
+//===----------------------------------------------------------------------===//
+//
+//                         BusTub
+//
+// extendible_hash_table.cpp
+//
+// Identification: src/container/hash/extendible_hash_table.cpp
+//
+// Copyright (c) 2022, Carnegie Mellon University Database Group
+//
+//===----------------------------------------------------------------------===//
+
+#include <cassert>
+#include <cstdlib>
+#include <functional>
+#include <list>
+#include <utility>
+
+#include "container/hash/extendible_hash_table.h"
+#include "storage/page/page.h"
+
+namespace bustub {
+
+template<typename K, typename V>
+ExtendibleHashTable<K, V>::ExtendibleHashTable(size_t bucket_size)
+    : global_depth_(0), bucket_size_(bucket_size), num_buckets_(1)
+{
+    // Create the first bucket and store it in a shared_ptr
+  dir_.push_back(std::make_shared<Bucket>(bucket_size_, global_depth_));
+}
+
+template <typename K, typename V>
+auto ExtendibleHashTable<K, V>::IndexOf(const K &key) -> size_t {
+  int mask = (1 << global_depth_) - 1;
+  return std::hash<K>()(key) & mask;
+}
+
+template <typename K, typename V>
+auto ExtendibleHashTable<K, V>::GetLocalHighBit(int dir_index) -> int {
+  return 1U << (GetLocalDepth(dir_index) - 1) ;
+}
+
+template <typename K, typename V>
+auto ExtendibleHashTable<K, V>::GetGlobalDepth() const -> int {
+  std::scoped_lock<std::mutex> lock(latch_);
+  return GetGlobalDepthInternal();
+}
+
+template <typename K, typename V>
+auto ExtendibleHashTable<K, V>::GetGlobalDepthInternal() const -> int {
+  return global_depth_;
+}
+
+template <typename K, typename V>
+auto ExtendibleHashTable<K, V>::GetLocalDepth(int dir_index) const -> int {
+  std::scoped_lock<std::mutex> lock(latch_);
+  return GetLocalDepthInternal(dir_index);
+}
+
+template <typename K, typename V>
+auto ExtendibleHashTable<K, V>::GetLocalDepthInternal(int dir_index) const -> int {
+  return dir_[dir_index]->GetDepth();
+}
+
+template <typename K, typename V>
+auto ExtendibleHashTable<K, V>::GetNumBuckets() const -> int {
+  std::scoped_lock<std::mutex> lock(latch_);
+  return GetNumBucketsInternal();
+}
+
+template <typename K, typename V>
+auto ExtendibleHashTable<K, V>::GetNumBucketsInternal() const -> int {
+  return num_buckets_;
+}
+
+template <typename K, typename V>
+auto ExtendibleHashTable<K, V>::Find(const K &key, V &value) -> bool {
+  std::scoped_lock<std::mutex> lock(latch_);
+  size_t index = IndexOf(key);
+  return dir_[index]->Find(key, value);
+}
+
+template <typename K, typename V>
+auto ExtendibleHashTable<K, V>::Remove(const K &key) -> bool {
+  std::scoped_lock<std::mutex> lock(latch_);
+  size_t index = IndexOf(key);
+  return dir_[index]->Remove(key);
+}
+
+template <typename K, typename V>
+void ExtendibleHashTable<K, V>::Insert(const K &key, const V &value) {
+  std::scoped_lock<std::mutex> lock(latch_);
+  size_t index = IndexOf(key);
+
+  if (dir_[index]->Insert(key, value)) {
+    return;
+  }
+
+  int local_depth = dir_[index]->GetDepth();
+
+  // double entries of the directory (and inc global depth)
+  if (local_depth == global_depth_) {
+    size_t dir_size = dir_.size();
+    dir_.reserve(2 * dir_size);
+    std::copy_n(dir_.begin(), dir_size, std::back_inserter(dir_));
+    ++global_depth_;
+  }
+
+  auto bucket_0 = std::make_shared<Bucket>(bucket_size_, local_depth + 1);
+  auto bucket_1 = std::make_shared<Bucket>(bucket_size_, local_depth + 1);
+  ++num_buckets_;
+  int local_depth_mask = 1 << local_depth;
+
+  // re-distribute the pairs back in the right buckets
+  for (const auto &[k, v] : dir_[index]->GetItems()) {
+    size_t item_hash = std::hash<K>()(k);
+    if (static_cast<bool>(item_hash & local_depth_mask)) {
+      bucket_1->Insert(k, v);
+    } else {
+      bucket_0->Insert(k, v);
+    }
+  }
+
+  // Adjust the directory pointers
+  for (size_t i = (std::hash<K>()(key) & (local_depth_mask - 1)); i < dir_.size(); i += local_depth_mask) {
+    if (static_cast<bool>(i & local_depth_mask)) {
+      dir_[i] = bucket_1;
+    } else {
+      dir_[i] = bucket_0;
+    }
+  }
+  latch_.unlock();
+  Insert(key, value);
+  latch_.lock();
+}
+
+
+//===--------------------------------------------------------------------===//
+// Bucket
+//===--------------------------------------------------------------------===//
+template <typename K, typename V>
+ExtendibleHashTable<K, V>::Bucket::Bucket(size_t array_size, int depth) : size_(array_size), depth_(depth) {}
+
+template <typename K, typename V>
+auto ExtendibleHashTable<K, V>::Bucket::Find(const K &key, V &value) -> bool {
+  for (const auto& [k, v] : list_){
+    if (key == k){
+      value = v;
+      return true; 
+    }
+  }
+  return false;
+}
+
+template <typename K, typename V>
+auto ExtendibleHashTable<K, V>::Bucket::Remove(const K &key) -> bool {
+  // Need the iterator for erase (list)
+  for (auto it = list_.begin(); it != list_.end(); ++it) {
+    if (key == it->first) {
+        list_.erase(it);
+        return true;
+    }
+}
+  return false;
+}
+
+template <typename K, typename V>
+auto ExtendibleHashTable<K, V>::Bucket::Insert(const K &key, const V &value) -> bool {
+  if (IsFull()){
+    return false;
+  }
+
+  for (auto& [k, v] : list_){
+    if (key == k){
+      v = value;
+      return true;
+    }
+  }
+  
+  list_.emplace_back(key, value);
+  return true;
+}
+
+template class ExtendibleHashTable<page_id_t, Page *>;
+template class ExtendibleHashTable<Page *, std::list<Page *>::iterator>;
+template class ExtendibleHashTable<int, int>;
+// test purpose
+template class ExtendibleHashTable<int, std::string>;
+template class ExtendibleHashTable<int, std::list<int>::iterator>;
+
+}  // namespace bustub
